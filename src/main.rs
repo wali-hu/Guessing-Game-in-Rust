171e4dfe110f81b08e3accf73588e1dbf4cee326
Muhammad Abdullah <abdullah.se4th@gmail.com>
2025-02-10 15:00:07 +0500

    gets a guess from the user and prints it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Salaam there, Welcome to the guessing game!");
+    println!("Salaam there, Guesss a number!");
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
